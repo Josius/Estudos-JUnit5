6fcb77dca442e0faa6e14da4e2b7c2a571526a86
Josius <josius.amaro@gmail.com>
2023-07-21 11:38:09 -0300

    aula 05 completa

--- junit5-estudos/src/test/java/junit5tests/ParameterizedTests.java
+++ junit5-estudos/src/test/java/junit5tests/ParameterizedTests.java
@@ -1,6 +1,7 @@
 package junit5tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -15,12 +16,43 @@ public class ParameterizedTests {
 	}
 
 	@ParameterizedTest
-	// as 3 annotations abaixo devem ser usadas somente com métodos que recebem um único parâmetro
+	// as 3 annotations abaixo devem ser usadas somente com métodos que recebem um
+	// único parâmetro
 	@NullAndEmptySource // passa uma string null e uma string vazia
 	// @NullSource // passa um string null, também não usar com tipos primitivos
-	// @EmptySource // string vazia passada para o @ValueSource, não usar com tipos primitivos, como no método acima
+	// @EmptySource // string vazia passada para o @ValueSource, não usar com tipos
+	// primitivos, como no método acima
 	@ValueSource(strings = { "firstString", "segundaString" })
 	void stringValues(String theParam) {
 		System.out.println("theParam: " + theParam);
+	}
+
+	@ParameterizedTest
+	// abaixo, como o método recebe 2 parâmetros, enviamos string separada por vírgula
+	@CsvSource(value = { "ciclops,wolverine", "rogue,storm", "beast,gambit", "jubilee, jean grey", "professor,X" }) 
+	void csvSource_doubleString(String param_1, String param_2) {
+		System.out.println("param_1: " + param_1 + ", param_2: " + param_2);
+	}
+
+	@ParameterizedTest
+	// abaixo, não precisamos especificar o tipo do dado enviado, somente separar por vírgula dentro de aspas duplas e a ordem deve estar como no método que receberá
+	@CsvSource(value = {"ciclops,32,true", "rogue,21,false", "beast,5,true"})
+	// abaixo, necessário especificar o tipo de parâmetro a ser recebido
+	void csvSource_StringIntBoolean(String param_1, int param_2, boolean param_3) {
+		System.out.println("param_1: " + param_1 + ", param_2: " + param_2 + ", param_3: " + param_3);
+	}
+
+	// se a string passada tiver uma vírgula em seu texto, precisamos passar um escape -> ('') para não entender essa vírgula como um separador para o método
+	@ParameterizedTest
+	@CsvSource(value = {"ciclops,'Summers, Scott'", "wolverine,'X,Arma'"})
+	void csvSource_StringWithComa(String param_1, String param_2){
+		System.out.println("param_1: " + param_1 + ", param_2: " + param_2);
+	}
+
+	// se em algum cenário não pudermos usar a vírgula como delimitador de parâmetros, podemos mudar o tipo de delimitador como um atributo da annotation
+	@ParameterizedTest
+	@CsvSource(value = { "ciclops?wolverine", "rogue?storm", "beast?gambit", "jubilee?jean grey", "professor?X" }, delimiter = '?') 
+	void csvSource_StringWithDiffDelimiter(String param_1, String param_2) {
+		System.out.println("param_1: " + param_1 + ", param_2: " + param_2);
 	}
 }
